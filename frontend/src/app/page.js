56f0df29cd58916d2d73e5a8cb73f50ac9af8390
Roman Pavlosiuk <rpavlosiuk@gmail.com>
2025-04-26 22:37:53 +0300

    Added DCF

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,15 @@
 import React from "react";
 import FarmLossForm from "./farm-loss-form/FarmLossForm";
+import FormSection from "./chart-section/form-section";
+import ChartSection from "./chart-section/chart-section";
 
 export default function Home() {
+  const [data, setData] = useState([]);
   return (
     <main>
       <FarmLossForm />
+      <FormSection  onSave={setData}/>
+      <ChartSection data={data} />
     </main>
   );
 }
