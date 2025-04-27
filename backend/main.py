8fa2a5cb1d3df76e198e186b30ac89f8e3f028f2
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2025-04-27 07:31:15 +0300

    fixes

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,9 @@ app = FastAPI(title="Agricultural Losses API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
