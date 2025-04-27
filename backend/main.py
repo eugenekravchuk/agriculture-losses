cafb62c0db9abd1565fd3c42e36a988a00255a68
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2025-04-27 07:12:10 +0300

    fixes

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ app = FastAPI(title="Agricultural Model API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
